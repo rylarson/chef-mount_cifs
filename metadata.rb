aee88b84ef669d8b1bcd08f80bf8fd67d615061b
Ryan Larson <rlarson@tripwire.com>
2014-10-03 19:11:48 -0700

    Bump version to 0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.mango.larson@gmail.com'
 license          'MIT'
 description      'Installs requires packages to enable mounting over CIFS'
 long_description description
-version          '0.1.0'
+version          '0.2.0'
 
 depends "apt", '>= 0.0.0' 
 
